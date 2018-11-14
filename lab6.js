3726757d2389038c4cd46d6dcfbaef8b28263098
Richardo Stewart <richardo.stewart@mymona.uwi.edu>
2018-11-14 04:09:48 +0000

    Ex 4

--- lab6.js
+++ lab6.js
@@ -1,17 +1,25 @@
 
-function definition()
+function definition(search)
 {
+    var word = document.getElementById("search").value.toLowerCase();
     var http = new XMLHttpRequest();
     
     http.onreadystatechange = function()
     {
         if (http.readyState == 4 && http.readyState == 200)
         {
-            alert(http.responseText);
+            parseData(this,search,word);
         }
     };
     
     http.open("GET","request.php", true);
     http.send();
+}
+
+function parseData(xml,search, word)
+{
+    var doc = xml.responseXML;
+    var notfound = true
+    var x = doc.getElementsByTagName("definition");
     
 }
